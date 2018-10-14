dc53544ce603a5515b3ea31dbf3122fe40a5cf4c
YzahFe <khariza.gapuz17@gmail.com>
2018-10-14 17:45:09 +0800

    Students
    Students

---
+++ application/views/students/edit-student.php
@@ -0,0 +1,127 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-8 p-r-0 title-margin-right">
+            <div class="page-header">
+                <div class="page-title">
+                    <h1><i class="ti-plus"></i> Edit Student</h1>
+                </div>
+            </div>
+        </div>
+        <!-- /# column -->
+        <div class="col-lg-4 p-l-0 title-margin-left">
+            <div class="page-header">
+                <div class="page-title">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                        <li class="breadcrumb-item">Students</li>
+                        <li class="breadcrumb-item active">Edit Student</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <!-- /# column -->
+    </div>
+    <section id="main-content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form>
+                            <div class="card-title">
+                                <span>Student Information</span>
+                            </div>
+                            <div class="row mt-1">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">School ID:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">First Name:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Middle Name:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Last Name:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Address:</label>
+                                        <div class="col-sm-9">
+                                            <textarea class="form-control" rows="20"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Phone:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Email:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-title">
+                                <span>Guardian Information</span>
+                            </div>
+                            <div class="row mt-1">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">First Name:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Middle Name:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Last Name:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Email:</label>
+                                        <div class="col-sm-9">
+                                            <input class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-right">
+                                        <a class="btn btn-danger" href="<?=base_url()?>students"><i class="ti-arrow-circle-left"></i> Back</a>
+                                        <button class="btn btn-default" type="submit"><i class="ti-save"></i> Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<script src="<?= base_url()?>assets/modules/js/teachers.js"></script>
