b23ed2f9dd502e7628ce8b5f21e7bfb34d1767d7
jdtolentino <jltolentino0106@gmail.com>
2018-10-16 07:00:54 +0800

    Teachers
    Update on crud

--- application/views/teachers/teachers.php
+++ application/views/teachers/teachers.php
@@ -23,104 +23,66 @@
     <section id="main-content">
         <div class="row">
             <div class="col-md-12">
-                <div class="card card-table">
-                    <div class="card-body">
-                        <div class="search-box">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="ti-search"></i></span>
-                                </div>
-                                <input type="text" class="form-control search-box-input" placeholder="Search">
-                                <div class="input-group-prepend input-group-left">
-                                    <a href="<?=base_url();?>teachers/add" class="btn btn-standard btn-sm"><i class="ti-plus"></i> <span>New</span></a>
-                                    <a href="<?=base_url();?>teachers/edit" class="btn btn-standard btn-sm"><i class="ti-pencil-alt"></i> <span>Edit</span></a>
-                                    <!-- <button class="btn btn-standard btn-sm"><i class="ti-pencil-alt"></i> <span>Edit</span></button> -->
-                                    <button class="btn btn-standard btn-sm"><i class="ti-trash"></i> <span>Delete</span></button>
+                <form action="teachers/delete" method="post">
+                    <div class="card card-table">
+                        <div class="card-body">
+                            <div class="search-box">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ti-search"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control search-box-input" placeholder="Search">
+                                    <div class="input-group-prepend input-group-left">
+                                        <a href="<?=base_url();?>teachers/add" class="btn btn-standard btn-sm"><i class="ti-plus"></i> <span>New</span></a>
+                                        <a href="<?=base_url();?>teachers/edit" class="btn btn-standard btn-sm"><i class="ti-pencil-alt"></i> <span>Edit</span></a>
+                                        <!-- <button class="btn btn-standard btn-sm"><i class="ti-pencil-alt"></i> <span>Edit</span></button> -->
+                                        <button type="submit" class="btn btn-standard btn-sm"><i class="ti-trash"></i> <span>Delete</span></button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="table-responsive">
-                            <table class="table table-hovered table-striped datatables">
-                                <thead>
-                                    <tr>
-                                        <th><input type="checkbox" ></th>
-                                        <th>Name</th>
-                                        <th>Address</th>
-                                        <th>Phone No.</th>
-                                        <th>Subject</th>
-                                        <th>Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td><input type="checkbox" ></td>
-                                        <td>Jan Lawrence D.C. Tolentino</td>
-                                        <td>Quezon City</td>
-                                        <td>09251232323</td>
-                                        <td>Math</td>
-                                        <td><span class="badge badge-pill badge-success">Registered</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox" ></td>
-                                        <td>Khariza Fe L. Gapuz</td>
-                                        <td>Mandaluyong City</td>
-                                        <td>09251232323</td>
-                                        <td>Science</td>
-                                        <td><span class="badge badge-pill badge-danger">Not Registered</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox" ></td>
-                                        <td>Jan Lawrence D.C. Tolentino</td>
-                                        <td>Quezon City</td>
-                                        <td>09251232323</td>
-                                        <td>Math</td>
-                                        <td><span class="badge badge-pill badge-success">Registered</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox" ></td>
-                                        <td>Khariza Fe L. Gapuz</td>
-                                        <td>Mandaluyong City</td>
-                                        <td>09251232323</td>
-                                        <td>Science</td>
-                                        <td><span class="badge badge-pill badge-danger">Not Registered</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox" ></td>
-                                        <td>Jan Lawrence D.C. Tolentino</td>
-                                        <td>Quezon City</td>
-                                        <td>09251232323</td>
-                                        <td>Math</td>
-                                        <td><span class="badge badge-pill badge-success">Registered</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox" ></td>
-                                        <td>Khariza Fe L. Gapuz</td>
-                                        <td>Mandaluyong City</td>
-                                        <td>09251232323</td>
-                                        <td>Science</td>
-                                        <td><span class="badge badge-pill badge-danger">Not Registered</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox" ></td>
-                                        <td>Jan Lawrence D.C. Tolentino</td>
-                                        <td>Quezon City</td>
-                                        <td>09251232323</td>
-                                        <td>Math</td>
-                                        <td><span class="badge badge-pill badge-success">Registered</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox" ></td>
-                                        <td>Khariza Fe L. Gapuz</td>
-                                        <td>Mandaluyong City</td>
-                                        <td>09251232323</td>
-                                        <td>Science</td>
-                                        <td><span class="badge badge-pill badge-danger">Not Registered</span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <div class="table-responsive">
+                                <table class="table table-hovered table-striped datatables">
+                                    <thead>
+                                        <tr>
+                                            <th><input type="checkbox" ></th>
+                                            <th>Name</th>
+                                            <th>Address</th>
+                                            <th>Phone No.</th>
+                                            <th>Subject</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><input type="checkbox" name="teacherId[]" value="1"></td>
+                                            <td>Jan Lawrence D.C. Tolentino</td>
+                                            <td>Quezon City</td>
+                                            <td>09251232323</td>
+                                            <td>Math</td>
+                                            <td><span class="badge badge-pill badge-success">Registered</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td><input type="checkbox" name="teacherId[]" value="1"></td>
+                                            <td>Khariza Fe L. Gapuz</td>
+                                            <td>Mandaluyong City</td>
+                                            <td>09251232323</td>
+                                            <td>Science</td>
+                                            <td><span class="badge badge-pill badge-danger">Not Registered</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td><input type="checkbox" name="teacherId[]" value="1"></td>
+                                            <td>Jan Lawrence D.C. Tolentino</td>
+                                            <td>Quezon City</td>
+                                            <td>09251232323</td>
+                                            <td>Math</td>
+                                            <td><span class="badge badge-pill badge-success">Registered</span></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </section>
