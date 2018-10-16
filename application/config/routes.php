47f40edafdc7df3abdb933d479bd65caa439c536
jdtolentino <jltolentino0106@gmail.com>
2018-10-16 23:37:58 +0800

    Update application/config/routes.php
    Update route for teacher module

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'dashboard';
-$route['teachers/(:any)'] = 'teachers/index/$1';
+$route['teachers/(:any)'] = 'teachers/index/$1/';
+$route['teachers/(:any)/(:any)'] = 'teachers/index/$1/$2';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
