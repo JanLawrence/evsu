9b92406e553417c948a2e9bf378ec570d215daff
jdtolentino <jltolentino0106@gmail.com>
2018-10-20 22:19:26 +0800

    Config
    Config for teachers module

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'dashboard';
+$route['teachers/(:any)'] = 'teachers/index/$1/';
+$route['teachers/(:any)/(:any)'] = 'teachers/index/$1/$2';
+$route['students/(:any)'] = 'students/index/$1/';
+$route['students/(:any)/(:any)'] = 'students/index/$1/$2';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
