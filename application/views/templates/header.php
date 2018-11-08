37b9caec1e7ff4e24e8015b6948761ba4f965c11
jdtolentino <jltolentino0106@gmail.com>
2018-11-08 22:02:10 +0800

    update colors

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,3 +1,6 @@
+<?php  
+    $user = $this->session->userdata['user'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,7 +22,15 @@
         <link href="<?= base_url(); ?>assets/css/lib/helper.css" rel="stylesheet">
         <link href="<?= base_url(); ?>assets/css/lib/select2/select2.min.css" rel="stylesheet">
         <link href="<?= base_url(); ?>assets/css/style.css" rel="stylesheet">
-        <link href="<?= base_url(); ?>assets/css/style-me.css" rel="stylesheet">
+        <?php if( $user->user_type == 'student'): ?>
+        <link href="<?= base_url(); ?>assets/css/style-me-student.css" rel="stylesheet">
+        <?php elseif( $user->user_type == 'parent'): ?>
+        <link href="<?= base_url(); ?>assets/css/style-me-parent.css" rel="stylesheet">
+        <?php elseif( $user->user_type == 'teacher'): ?>
+        <link href="<?= base_url(); ?>assets/css/style-me-teacher.css" rel="stylesheet">
+        <?php elseif( $user->user_type == 'admin'): ?>
+        <link href="<?= base_url(); ?>assets/css/style-me-admin.css" rel="stylesheet">
+        <?php endif; ?>
         <script src="<?= base_url();?>assets/js/lib/jquery.min.js"></script>
         <script>
             var URL = "<?= base_url(); ?>"
