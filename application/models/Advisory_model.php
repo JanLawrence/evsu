8adaa9cb591872cb93a474c93cfe7f5672cbc977
jdtolentino <jltolentino0106@gmail.com>
2019-02-13 07:03:47 +0800

    Update Advisory_model.php

--- application/models/Advisory_model.php
+++ application/models/Advisory_model.php
@@ -12,7 +12,8 @@ class Advisory_model extends CI_Model{
          ->join('tbl_credentials c', 'c.user_id = s.id AND c.user_type = "student"', 'inner')
          ->join('tbl_students_grade sg', 'sg.student_id = s.id', 'left')
          ->where('sec.teacher_id' , $user->user_id)
-         ->where('c.confirm' , 'yes');
+         ->where('c.confirm' , 'yes')
+         ->group_by('s.id');
 
         //  ->group_by('ts.teacher_id' , 's.id');
         $query = $this->db->get(); // get results of query
