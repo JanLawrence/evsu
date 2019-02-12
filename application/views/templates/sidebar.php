18aeb4836c34cec67cfa3f363f6f99bdfe13c72f
jdtolentino <jltolentino0106@gmail.com>
2019-02-12 20:52:49 +0800

    Update sidebar.php

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -18,11 +18,12 @@
                 <?php if($user->user_type == 'admin'):?>
                     <li><a href="<?= base_url(); ?>dashboard"><i class="ti-dashboard"></i> Dashboard</a></li>
                     <li><a href="<?= base_url(); ?>teachers"><i class="ti-user"></i> Teachers</a></li>
+                    <li><a href="<?= base_url(); ?>section"><i class="ti-comments"></i> Sections</a></li>
                     <li><a href="<?= base_url(); ?>subjects"><i class="ti-book"></i> Subjects</a></li>
+                    <li><a href="<?= base_url(); ?>students"><i class="ti-user"></i> Students</a></li>
                     <!-- <li><a href="<?= base_url(); ?>students/studentlog"><i class="ti-agenda"></i> Student Log</a></li> -->
                     <li><a href="<?= base_url(); ?>announcements/announcementList"><i class="ti-announcement"></i> Announcements</a></li>
                     <li><a href="<?= base_url(); ?>feedbacks/feedbackList"><i class="ti-comments"></i> Feedbacks</a></li>
-                    <li><a href="<?= base_url(); ?>section"><i class="ti-comments"></i> Sections</a></li>
                     <li><a href="<?= base_url(); ?>school_year"><i class="ti-comments"></i> School Year</a></li>
                     <li><a href="<?= base_url(); ?>admingrade"><i class="ti-book"></i> Grades</a></li>
                     <?php if($dataUser[0]->admin_type=='admin'):?>
@@ -37,7 +38,6 @@
                     <li><a href="<?= base_url(); ?>announcements"><i class="ti-announcement"></i> Announcements</a></li>
                     <li><a href="<?= base_url(); ?>grades/add"><i class="ti-clipboard"></i> Grades</a></li>
                     <!-- <li><a href="<?= base_url(); ?>students/studentattendance"><i class="ti-agenda"></i> Student Attendance</a></li> -->
-                    <li><a href="<?= base_url(); ?>students"><i class="ti-user"></i> Students</a></li>
                     <li><a href="<?= base_url(); ?>inbox"><i class="ti-comments"></i> Inbox</a></li>
                     <li><a href="<?= base_url(); ?>advisory"><i class="ti-write"></i> Class Cards</a></li>
                 <?php endif;?>
